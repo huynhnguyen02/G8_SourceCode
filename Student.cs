cdb52aeb1a12569bb09c35befe7b57585b44e5e7
huynhnguyen02 <88275369+huynhnguyen02@users.noreply.github.com>
2021-12-06 22:18:22 +0700

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Student
+{
+    private string id;
+    private string name;
+    private float grade;
+    public string getID()
+    {
+        return this.id;
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+    public float getGrade()
+    {
+        return this.grade;
+    }
+    public Student(string id, string name, float grade)
+    {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+    public override string ToString()
+    {
+        return "Student(" + id + "," + name + "," + grade + ")";
+    }
+}
+
